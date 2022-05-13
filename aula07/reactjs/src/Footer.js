bd331199af0b5d21c1337a996818dac07f14ff5d
Gabriel <95183526+ImGabriell@users.noreply.github.com>
2022-05-12 21:16:55 -0300

    Add files via upload

---
+++ aula07/reactjs/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <>Todos os direitos reservados para o monstro Gabriell</>
+      <p>Antônio parceiro</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ aula07/reactjs/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1>Teste de Header </h1>
+      <p>Novo Parágrafo</p>
+    </div>
+  );
+}
+
+export default Header;
